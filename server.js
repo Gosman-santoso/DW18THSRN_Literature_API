b4971b3f7b0cbe0e70d936cc0946775bac9bdeb5
Gosman-santoso <gosmansantoso18@gmail.com>
2020-10-31 00:05:24 +0700

    Beta 1.0.1

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ app.use("/public/thumbnails", express.static("public/thumbnails"));
 
 app.use("/api/v2/", router);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
