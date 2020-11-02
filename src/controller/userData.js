158161ce2a240bd92ba0483bf8425779488b101f
Gosman-santoso <gosmansantoso18@gmail.com>
2020-11-02 09:59:00 +0700

    bug fixing

--- src/controller/userData.js
+++ src/controller/userData.js
@@ -4,6 +4,28 @@ exports.getUsers = async(req, res) => {
     try {
         const users = await User.findAll();
 
+        // {
+        //     include: [{
+        //             model: Literature,
+        //             as: "literature"
+        //         },
+        //         {
+        //             model: Library,
+        //             as: "library",
+        //             include: [{
+        //                 model: Literature,
+        //                 as: "literature",
+        //                 attributes: {
+        //                     exclude: ["createdAt", "updatedAt", "userId", "UserId"]
+        //                 }
+        //             }]
+        //         }
+        //     ],
+        //     attributes: {
+        //         exclude: ["password", "createdAt", "updatedAt"]
+        //     }
+        // }
+
         res.send({
             message: "User Succsesfully Loaded",
             data: { users }
